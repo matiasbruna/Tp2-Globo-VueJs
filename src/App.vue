15e3136f7708388170ea3417b5f5afcf3f665a9e
Matias <matias.b.dev@gmail.com>
2023-06-10 13:54:37 -0300

    cree un componente nuevo con el codigo de las publicaciones y modifique el el App.vue para agregar las clases de bootstrap que contienen los componentes

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
   <CabeceraGlobo msg = ""/>
-  <PerfilGlobo/>
-  <CuerpoGlobo/>
-  <SobreMiGlobo/>
 
+  <PerfilGlobo/>
+
+  <div class="container mt-4 p-0">
+    <div class="row">
+        <div class="col-7">
+            <PublicacionesGlobo/>
+        </div>
+        <div class="col-5">
+          <SobreMiGlobo/>         
+        </div>
+    </div>
+  </div>
   
 </template>
 
 <script>
 import  CabeceraGlobo from './components/CabeceraGlobo.vue'
-import CuerpoGlobo from './components/CuerpoGlobo.vue';
 import PerfilGlobo from './components/PerfilGlobo.vue';
+import PublicacionesGlobo from './components/PublicacionesGlobo.vue';
 import SobreMiGlobo from './components/SobreMiGlobo.vue';
-
-
 
 
 export default {
   name: 'App',
   components: {
     CabeceraGlobo,
-    CuerpoGlobo,
     PerfilGlobo,
     SobreMiGlobo,
+    PublicacionesGlobo
 }
 }
 </script>
